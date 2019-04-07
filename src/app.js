8c1c665d942e973bbd46912e587481b86e30126b
Zeeshan Syed <zsyed89@gmail.com>
2019-04-07 12:00:44 -0400

    Added firebase login/logout capabilities

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null };
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyBsTlpb4M2wXVamqJ0Yx6h7jt-sjLI5098',
@@ -14,13 +16,36 @@ class App extends Component {
             storageBucket: 'auth-3d694.appspot.com',
             messagingSenderId: '125090724831'
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>;
+            case false:
+                return <LoginForm />;
+            default:
+                return (
+                    <View>
+                        <Spinner size="large" />
+                    </View>
+                );
+        }
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
